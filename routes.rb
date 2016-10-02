33386f01d3d37c93d2efa378fd26d4f419f52909
Jared <jaredcmurphy@gmail.com>
2016-10-02 17:46:07 -0400

    add band page

--- routes.rb
+++ routes.rb
@@ -2,4 +2,8 @@ class MightyShady < Sinatra::Base
   get "/" do
     erb :"index"
   end
+
+  get "/about" do
+    erb :"about"
+  end
 end
---
+++ seeds.rb
@@ -0,0 +1,71 @@
+venues
+# Des moines
+"The Underground",
+"Woolys",
+"Vaudeville Mews",
+"Gas Lamp",
+"El Bait Shop",
+"Des Moines Farmers' Market",
+"Star Bar",
+"Whiskey Dixx",
+"The House of Bricks",
+"Val Air Ballroom",
+"Beechwood Lounge",
+"Raygun Teeshirts",
+"Marque Lounge",
+"Lot 33",
+"The Flying Moose",
+"Bombay Bicycle Club"
+
+# Indianola
+"Zoo Bar",
+"Simpson College",
+# Ames
+"DG's Taphouse",
+"Iowa Music Store",
+# Boone
+"Shenanigans Event Center",
+#Marshalltown
+"The Oasis",
+# Iowa City
+"The Yacht Club",
+"Gabe's Oasis",
+"The Mill",
+# Cedar Rapids
+"Just Jules",
+"Parlor City",
+# Mason City
+"Lorados",
+"Murmur",
+# Minneapolis
+"Kathy's Pub",
+"Driftwood Char Bar",
+#Nevada
+"6th Street Pub",
+# Dubuque
+"The Lift"
+
+
+
+festivals
+"Riverside Jam",
+"Family Gathering",
+"Campustown Summerfest",
+"Summer Camp Music Festival",
+"Briar Patch",
+"Adventure Camp Music Festival",
+"Shagstock",
+"Dangerfest",
+"8035",
+"Wakarusa",
+"Love Fest in the Midwest"
+
+# radio and television
+"KCWI Great Day Morning Show",
+"IPR Studio One",
+"IPR Java Blend"
+
+# Accolades
+"SXSW Compilation Winner",
+"Waka Winter Classic Winner",
+"Little Big Fest Community Chosen Artist"
